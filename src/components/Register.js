f14777c062e57808399f8aa908b3900dada32f1a
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-03-27 23:07:57 +0300

    feat: add states and handle submit in Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,40 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
         
 function Register() {
-  // const currentUser = useContext(CurrentUserContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  // const [name, setName] = useState('');
-  // const [description, setDescription] = useState('');
-  
-  // useEffect(() => {
-  //   if (currentUser) {
-  //     setName(currentUser.name);
-  //     setDescription(currentUser.about);
-  //   }
-  // }, [currentUser, isOpen]);
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      setEmail(e.target.value);
+    }
+    if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
+  }
 
-  // function handleNameChange(e) {
-  //   setName(e.target.value);
-  // }
-  // function handleDescriptionChange(e) {
-  //   setDescription(e.target.value);
-  // }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-
-  //   onUpdateUser({
-  //     name,
-  //     about: description,
-  //   });
-  // }
+  }
 
   return (
     <section className="auth">
-      <form className='form form_type_register' name='register' onSubmit noValidate>
+      <form className='form form_type_register' name='register' onSubmit={handleSubmit} noValidate>
         <h2 className="form__title form__title_type_auth">Регистрация</h2>
         <fieldset className="form__container form__container_type_auth">
-          <input 
+          <input
+            onChange={handleChange}
+            value={email}
             id="email-input"
             className="form__input form__input_type_auth"
             type="email"
@@ -43,7 +35,9 @@ function Register() {
             required
           />
           {/* <span className="login-input-error form__input-error"></span> */}
-          <input 
+          <input
+            onChange={handleChange}
+            value={password}
             id="password-input"
             className="form__input form__input_type_auth"
             type="password"
@@ -55,7 +49,7 @@ function Register() {
         </fieldset>
         <button className="form__button form__button_type_submit-login" type="submit" name="submit" value='Зарегистрироваться'>Зарегистрироваться</button>
       </form>
-      <p className='auth__login-tip'>Уже зарегистрированы? Войти</p>
+      <p className='auth__login-tip'>Уже зарегистрированы? <Link to={`./sign-in`} className="auth__login-link">Войти</Link></p>
     </section>
   );
 }
