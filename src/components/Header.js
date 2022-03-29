799bcc17f237e92e8084c071c2130c8089a5bf98
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-03-29 16:14:54 +0300

    feat: header links layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import logo from '../images/logo-mesto.svg';
+import { Link, Route } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
       <img src={logo} alt="Логотип 'Место'" className="logo" />
+
+      <Route exact path="/">
+        <div className='header__user-info'>
+          <p className='header__email'>email@email.com</p>
+          <Link to={`./sign-in`} className="header__link">Выйти</Link>
+        </div> 
+      </Route>
+
+      <Route path="/sign-up">
+        <Link to={`./sign-in`} className="header__link">Войти</Link>
+      </Route>
+
+      <Route path="/sign-in">
+        <Link to={`./sign-up`} className="header__link">Регистрация</Link>
+      </Route>
     </header>
   );
 }
